ef9cd3db6b7603ffab7928278679178d3bf12920
sharad <sharadshetty2016@gmail.com>
2021-05-15 19:27:59 +0530

    initial modal

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Row, Col } from 'antd';
+import Modal from './partials/Modal';
 
 const Wrapper = styled(Row)`
   height: 100%;
 `;
 
 const Board = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [gameDifficulty, setGameDifficulty] = useState('');
+
+  useEffect(() => {
+    setIsModalVisible(true);
+  }, []);
+
+  const startGame = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <Wrapper>
       <Col span={12}>col-12</Col>
       <Col span={12}>col-12</Col>
+      <Modal
+        visible={isModalVisible}
+        value={gameDifficulty}
+        onChange={setGameDifficulty}
+        onSubmit={startGame}
+      />
     </Wrapper>
   );
 };
---
+++ src/components/Board/partials/Modal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Modal as AntdModal, Button, Radio } from 'antd';
+
+const Modal = ({ visible, value, onChange, onSubmit }) => {
+  return (
+    <AntdModal
+      visible={visible}
+      title="Choose difficulty"
+      closable={false}
+      footer={[
+        <Button
+          key="submit"
+          type="primary"
+          disabled={!value}
+          onClick={onSubmit}
+        >
+          Let's Go
+        </Button>,
+      ]}
+    >
+      <Radio.Group onChange={(e) => onChange(e.target.value)}>
+        <Radio.Button value="easy">Easy</Radio.Button>
+        <Radio.Button value="medium">Medium</Radio.Button>
+        <Radio.Button value="hard">Hard</Radio.Button>
+      </Radio.Group>
+    </AntdModal>
+  );
+};
+
+export default Modal;
